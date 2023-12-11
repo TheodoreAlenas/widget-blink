67ce80e4b4bc065c5429f252d3f1c471ca3033ae
Theodore Alenas <dimakopt732@gmail.com>
2023-12-11 14:11:21 +0200

    init a GC and a pixelmap, unused

--- main.c
+++ main.c
@@ -41,10 +41,12 @@ static void usage(const char *program_name) {
 }
 
 int main(int argc, char *argv[]) {
-	int screen;
+	int screen, w, h;
 	Display *dpy;
 	Window root;
 	XWindowAttributes wa;
+	Drawable drawable;
+	GC gc;
 	for (int i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "-v")) {
 			puts("albatpop-"VERSION);
@@ -68,5 +70,10 @@ int main(int argc, char *argv[]) {
 	if (!XGetWindowAttributes(dpy, root, &wa))
 		die("could not get embedding window attributes: 0x%lx",
 		    root);
+	w = wa.width;
+	h = wa.height / 4;
+	drawable = XCreatePixmap(dpy, root, w, h, DefaultDepth(dpy, screen));
+	gc = XCreateGC(dpy, root, 0, NULL);
+	XSetLineAttributes(dpy, gc, 1, LineSolid, CapButt, JoinMiter);
 	return 0;
 }
