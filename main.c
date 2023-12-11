6ed06850ead9e4e909f2fba908abda8a037c397c
Theodore Alenas <dimakopt732@gmail.com>
2023-12-11 16:13:09 +0200

    extracted int function that takes strings

--- main.c
+++ main.c
@@ -10,10 +10,7 @@
 #include <X11/Xutil.h>
 #include <X11/Xft/Xft.h>
 
-int percentage = 101;
-double timeout = 0.5;
-
-void die(const char *fmt, ...) {
+static void die(const char *fmt, ...) {
 	va_list ap;
 
 	va_start(ap, fmt);
@@ -30,18 +27,12 @@ void die(const char *fmt, ...) {
 	exit(1);
 }
 
-static void usage(const char *program_name) {
-	fprintf(stderr,
-			"Usage:\n"
-			"  %s -p 100 -t 1.5  to show 100%% for 1.5 sec,\n"
-			"  %s -v             for version information,\n"
-			"  %s                for default behavior\n"
-			,
-			program_name, program_name, program_name);
-	exit(1);
-}
+void albatwid_draw(const char *text,
+				   const char *fg_color,
+				   const char *bg_color,
+				   const char *charged_bg_color,
+				   double timeout_seconds) {
 
-int main(int argc, char *argv[]) {
 	int screen, w, h, charged;
 	Display *dpy;
 	Window root, win;
@@ -54,20 +45,7 @@ int main(int argc, char *argv[]) {
 	XGlyphInfo ext;
 	XSetWindowAttributes swa;
 	XClassHint ch = {"dmenu", "dmenu"};
-	for (int i = 1; i < argc; i++) {
-		if (!strcmp(argv[i], "-v")) {
-			puts("albatpop-"VERSION);
-			exit(0);
-		}
-		else if (i + 1 == argc)  /* the next arguments take values */
-			usage(argv[0]);
-		else if (!strcmp(argv[i], "-p"))
-			percentage = atoi(argv[++i]);
-		else if (!strcmp(argv[i], "-t"))
-			timeout = atof(argv[++i]);
-		else
-			usage(argv[0]);
-	}
+
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
 		fputs("warning: no locale support\n", stderr);
 	if (!(dpy = XOpenDisplay(NULL)))
@@ -86,15 +64,15 @@ int main(int argc, char *argv[]) {
 		die("error, cannot load monospace font\n");
 	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
 	                       DefaultColormap(dpy, screen),
-						   "#000e17", &bg))
+						   bg_color, &bg))
 		die("error, cannot allocate background color");
 	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
 	                       DefaultColormap(dpy, screen),
-						   "#004065", &bg_charged))
+						   charged_bg_color, &bg_charged))
 		die("error, cannot allocate background color");
 	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
 	                       DefaultColormap(dpy, screen),
-	                       "#afbcbf", &fg))
+	                       fg_color, &fg))
 		die("error, cannot allocate foreground color");
 	XSetForeground(dpy, gc, bg.pixel);
 	XFillRectangle(dpy, drawable, gc, 0, 0, w, h);
@@ -104,11 +82,11 @@ int main(int argc, char *argv[]) {
 	d = XftDrawCreate(dpy, drawable,
 					  DefaultVisual(dpy, screen),
 					  DefaultColormap(dpy, screen));
-	XftTextExtentsUtf8(dpy, xfont, (XftChar8 *)"69", 2, &ext);
+	XftTextExtentsUtf8(dpy, xfont, (XftChar8 *)text, strlen(text), &ext);
 	XftDrawStringUtf8(d, &fg, xfont,
 					  (w - ext.xOff) / 2,
 					  (h + xfont->ascent + xfont->descent) * 4/10,  /* fiddly */
-					  (XftChar8 *)"69", 2);
+					  (XftChar8 *)text, strlen(text));
 	swa.override_redirect = True;
 	swa.background_pixel = bg.pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
@@ -119,10 +97,44 @@ int main(int argc, char *argv[]) {
 	XMapRaised(dpy, win);
 	XCopyArea(dpy, drawable, win, gc, 0, 0, w, h, 0, 0);
 	XSync(dpy, False);
-	usleep(500000);
+	usleep(1000000L * timeout_seconds);
 
 	XftFontClose(dpy, xfont);
 	free(xfont);
+}
+
+static void usage(const char *program_name) {
+	fprintf(stderr,
+			"Usage:\n"
+			"  %s -p 100 -t 1.5  to show 100%% for 1.5 sec,\n"
+			"  %s -v             for version information,\n"
+			"  %s                for default behavior\n"
+			,
+			program_name, program_name, program_name);
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
+	int percentage = 69;
+	double timeout = 0.5;
+
+	for (int i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-v")) {
+			puts("albatpop-"VERSION);
+			exit(0);
+		}
+		else if (i + 1 == argc)  /* the next arguments take values */
+			usage(argv[0]);
+		else if (!strcmp(argv[i], "-p"))
+			percentage = atoi(argv[++i]);
+		else if (!strcmp(argv[i], "-t"))
+			timeout = atof(argv[++i]);
+		else
+			usage(argv[0]);
+	}
+	char p_txt[] = "12345678901234567890";
+	sprintf(p_txt, "%d", percentage);
+	albatwid_draw(p_txt, "#afbcbf", "#000e17", "#004065", timeout);
 
 	return 0;
 }
