273c8ed77917ac49a1adf9d8261cd679c20fde98
Theodore Alenas <dimakopt732@gmail.com>
2023-12-11 16:00:00 +0200

    mimicing the final appearance, without the soul

--- main.c
+++ main.c
@@ -42,15 +42,16 @@ static void usage(const char *program_name) {
 }
 
 int main(int argc, char *argv[]) {
-	int screen, w, h;
+	int screen, w, h, charged;
 	Display *dpy;
 	Window root, win;
 	XWindowAttributes wa;
 	Drawable drawable;
 	GC gc;
 	XftFont *xfont;
-	XftColor fg, bg;
+	XftColor fg, bg, bg_charged;
 	XftDraw *d = NULL;
+	XGlyphInfo ext;
 	XSetWindowAttributes swa;
 	XClassHint ch = {"dmenu", "dmenu"};
 	for (int i = 1; i < argc; i++) {
@@ -77,31 +78,41 @@ int main(int argc, char *argv[]) {
 		die("could not get embedding window attributes: 0x%lx",
 		    root);
 	w = wa.width;
-	h = wa.height / 4;
+	h = wa.height * 6/10;
 	drawable = XCreatePixmap(dpy, root, w, h, DefaultDepth(dpy, screen));
 	gc = XCreateGC(dpy, root, 0, NULL);
 	XSetLineAttributes(dpy, gc, 1, LineSolid, CapButt, JoinMiter);
-	if (!(xfont = XftFontOpenName(dpy, screen, "monospace:size=10")))
+	if (!(xfont = XftFontOpenName(dpy, screen, "Source Code Pro:size=300")))
 		die("error, cannot load monospace font\n");
 	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
 	                       DefaultColormap(dpy, screen),
-	                       "#aaffaa", &bg))
+						   "#000e17", &bg))
 		die("error, cannot allocate background color");
 	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
 	                       DefaultColormap(dpy, screen),
-	                       "#550000", &fg))
+						   "#004065", &bg_charged))
+		die("error, cannot allocate background color");
+	if (!XftColorAllocName(dpy, DefaultVisual(dpy, screen),
+	                       DefaultColormap(dpy, screen),
+	                       "#afbcbf", &fg))
 		die("error, cannot allocate foreground color");
 	XSetForeground(dpy, gc, bg.pixel);
 	XFillRectangle(dpy, drawable, gc, 0, 0, w, h);
+	charged = h * 30 / 100;
+	XSetForeground(dpy, gc, bg_charged.pixel);
+	XFillRectangle(dpy, drawable, gc, 0, h - charged, w, charged);
 	d = XftDrawCreate(dpy, drawable,
 					  DefaultVisual(dpy, screen),
 					  DefaultColormap(dpy, screen));
-	XftDrawStringUtf8(d, &fg,
-					  xfont, 0, 100, (XftChar8 *)"shit", 5);
+	XftTextExtentsUtf8(dpy, xfont, (XftChar8 *)"69", 2, &ext);
+	XftDrawStringUtf8(d, &fg, xfont,
+					  (w - ext.xOff) / 2,
+					  (h + xfont->ascent + xfont->descent) * 4/10,  /* fiddly */
+					  (XftChar8 *)"69", 2);
 	swa.override_redirect = True;
 	swa.background_pixel = bg.pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
-	win = XCreateWindow(dpy, root, 0, 0, w, h, 0,
+	win = XCreateWindow(dpy, root, 0, (wa.height - h) / 2, w, h, 0,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
 	XSetClassHint(dpy, win, &ch);
